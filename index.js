c5dab8b0a5d48e386ea829bb48837f3ceb47937b
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-11-25 15:14:37 -0800

    updated index.js with heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ auth(app); // Load authentication setup
 require("./passport"); // Initialize passport configuration
 
 mongoose
-  .connect("mongodb://localhost:27017/mymovieapi", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
