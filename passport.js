953d9dc5dc7ee25e1f54927a66daacc310e99ab5
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-12-03 15:38:35 -0800

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,32 +1,17 @@
-passport.use(
-    new LocalStrategy(
-      {
-        usernameField: 'Username',
-        passwordField: 'Password',
-      },
-      async (username, password, callback) => {
-        console.log(`${username} ${password}`);
-        await Users.findOne({ Username: username })
-        .then((user) => {
-          if (!user) {
-            console.log('incorrect username');
-            return callback(null, false, {
-              message: 'Incorrect username or password.',
-            });
-          }
-          if (!user.validatePassword(password)) {
-            console.log('incorrect password');
-            return callback(null, false, { message: 'Incorrect password.' });
-          }
-          console.log('finished');
-          return callback(null, user);
-        })
-        .catch((error) => {
-          if (error) {
-            console.log(error);
-            return callback(error);
-          }
-        })
-      }
-    )
-  );
+import passport from 'passport';
+
+require('./strategies/local.strategy')(); 
+
+const passportConfig = (app) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+  // stores user to session
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  // retrieves user from session
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+};
