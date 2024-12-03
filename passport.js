f0114a0b3810dbc20e9a028643a390bf95db7b94
Jamez <154967862+JamesH200@users.noreply.github.com>
2024-12-03 15:41:11 -0800

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,17 +1,46 @@
-import passport from 'passport';
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Users = require('./models/User'); // Adjust the path as necessary
 
-require('./strategies/local.strategy')(); 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password',
+    },
+    async (username, password, callback) => {
+      try {
+        console.log(`${username} ${password}`);
+        const user = await Users.findOne({ Username: username });
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, { message: 'Incorrect username or password.' });
+        }
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect password.' });
+        }
+        console.log('finished');
+        return callback(null, user);
+      } catch (error) {
+        console.log(error);
+        return callback(error);
+      }
+    }
+  )
+);
 
-const passportConfig = (app) => {
-  app.use(passport.initialize());
-  app.use(passport.session());
-  // stores user to session
-  passport.serializeUser((user, done) => {
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await Users.findById(id);
     done(null, user);
-  });
+  } catch (error) {
+    done(error, null);
+  }
+});
 
-  // retrieves user from session
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
-};
+module.exports = passport;
